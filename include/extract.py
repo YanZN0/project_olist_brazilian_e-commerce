e1799d95731b3fa728b2b7be8a64ed2c63fb01bd
Yan Robim <yanrobim@gmail.com>
2025-06-06 14:24:32 -0300

    Refatorando função, alterando caminho da pasta para a utilização da função na DAG Airflow.

--- include/extract.py
+++ include/extract.py
@@ -8,7 +8,7 @@ def extracting_and_reading_csv():
     Essa função tem como objetivo ler arquivos CSV do caminho recebido,
     trabalha com multiplos CSVs fazendo uma separação individual através de um dict de dataframes.
     '''
-    caminho = os.path.join('/usr', 'local', 'airflow', 'include', 'datasets')
+    caminho = os.path.join('/usr', 'local', 'airflow', 'include', 'raw_datasets')
     dataframes = {}             
     for arquivos in os.listdir(caminho):
         if arquivos.endswith('.csv'):
@@ -17,6 +17,7 @@ def extracting_and_reading_csv():
             extract = pd.read_csv(caminho_completo)
             dataframes[nome] = extract
     return dataframes
+
 
 
 
